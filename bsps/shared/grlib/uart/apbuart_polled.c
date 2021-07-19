6eb3a0b5f011c019ddf22aa9f13fa1b2e8391fbd
Sebastian Huber <sebastian.huber@embedded-brains.de>
2021-07-19 15:21:26 +0200

    bsps: Use new APBUART register block API

--- bsps/shared/grlib/uart/apbuart_polled.c
+++ bsps/shared/grlib/uart/apbuart_polled.c
@@ -1,45 +1,72 @@
-/*
- *  COPYRIGHT (c) 2010.
- *  Cobham Gaisler AB.
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+/**
+ * @file
  *
- *  The license and distribution terms for this file may be
- *  found in the file LICENSE in this distribution or at
- *  http://www.rtems.org/license/LICENSE.
+ * @ingroup DevGrlibIfApbuart
+ *
+ * @brief This source file contains the implementation of
+ *   apbuart_outbyte_wait(), apbuart_outbyte_polled(), and
+ *   apbuart_inbyte_nonblocking().
+ */
+
+/*
+ * Copyright (C) 2021 embedded brains GmbH (http://www.embedded-brains.de)
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <grlib/apbuart.h>
+#include <grlib/io.h>
 
-#include <rtems/score/cpuimpl.h>
+#include <rtems/score/io.h>
 
-void apbuart_outbyte_wait(const struct apbuart_regs *regs)
+void apbuart_outbyte_wait( const apbuart *regs )
 {
-  while ( (regs->status & APBUART_STATUS_TE) == 0 ) {
-    /* Lower bus utilization while waiting for UART */
-    _CPU_Instruction_no_operation();
-    _CPU_Instruction_no_operation();
-    _CPU_Instruction_no_operation();
-    _CPU_Instruction_no_operation();
-    _CPU_Instruction_no_operation();
-    _CPU_Instruction_no_operation();
-    _CPU_Instruction_no_operation();
-    _CPU_Instruction_no_operation();
+  while ( ( grlib_load_32( &regs->status ) & APBUART_STATUS_TE ) == 0 ) {
+    _IO_Relax();
   }
 }
 
-void apbuart_outbyte_polled(struct apbuart_regs *regs, char ch)
+void apbuart_outbyte_polled( apbuart *regs, char ch)
 {
-  apbuart_outbyte_wait(regs);
-  regs->data = (uint8_t) ch;
+  apbuart_outbyte_wait( regs );
+  grlib_store_32( &regs->data, (uint8_t) ch );
 }
 
-int apbuart_inbyte_nonblocking(struct apbuart_regs *regs)
+int apbuart_inbyte_nonblocking( apbuart *regs )
 {
-  /* Clear errors */
-  regs->status = ~APBUART_STATUS_ERR;
+  uint32_t status;
 
-  if ((regs->status & APBUART_STATUS_DR) == 0) {
+  status = grlib_load_32( &regs->status );
+
+  /* Clear errors, writes to non-error flags are ignored */
+  status &= ~( APBUART_STATUS_FE | APBUART_STATUS_PE | APBUART_STATUS_OV |
+    APBUART_STATUS_BR );
+  grlib_store_32( &regs->status, status );
+
+  if ( ( status & APBUART_STATUS_DR ) == 0 ) {
     return -1;
   }
 
-  return (uint8_t) regs->data;
+  return (int) APBUART_DATA_DATA_GET( grlib_load_32( &regs->data ) );
 }
