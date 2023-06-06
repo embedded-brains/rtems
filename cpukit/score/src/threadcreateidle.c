81bad46b945533c7dda5ca5bd63195454c049c04
Xavier Ferreira <xavier.ferreira@criticalsoftware.com>
2023-06-06 10:19:58 +0100

    RTEMS-SMP-CODE-VER-058 -- refer also to https://devel.rtems.org/ticket/4818.

--- cpukit/score/src/threadcreateidle.c
+++ cpukit/score/src/threadcreateidle.c
@@ -92,7 +92,7 @@ static void _Thread_Create_idle_for_CPU( Per_CPU_Control *cpu )
    *  WARNING!!! This is necessary to "kick" start the system and
    *             MUST be done before _Thread_Start is invoked.
    */
-  cpu->heir      =
+  cpu->heir      = idle;
   cpu->executing = idle;
 #if defined(RTEMS_SMP)
   cpu->ancestor = idle;
