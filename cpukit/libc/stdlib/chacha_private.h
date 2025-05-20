8d59167e58c88f7dd5aec477fe509fb43541aa40
Sebastian Huber <sebastian.huber@embedded-brains.de>
2025-05-20 04:03:37 +0200

    arc4random: Static assert chacha_encrypt_bytes()

--- cpukit/libc/stdlib/chacha_private.h
+++ cpukit/libc/stdlib/chacha_private.h
@@ -89,16 +89,19 @@ chacha_ivsetup(chacha_ctx *x,const u8 *iv)
   x->input[15] = U8TO32_LITTLE(iv + 4);
 }
 
+#define chacha_encrypt_bytes(x, m, c, bytes) do { \
+	_Static_assert((bytes) > 0, "chacha_encrypt_bytes"); \
+	chacha_do_encrypt_bytes(x, m, c, bytes); \
+} while (0)
+
 static void
-chacha_encrypt_bytes(chacha_ctx *x,const u8 *m,u8 *c,u32 bytes)
+chacha_do_encrypt_bytes(chacha_ctx *x,const u8 *m,u8 *c,u32 bytes)
 {
   u32 x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15;
   u32 j0, j1, j2, j3, j4, j5, j6, j7, j8, j9, j10, j11, j12, j13, j14, j15;
   u8 *ctarget = NULL;
   u8 tmp[64];
   u_int i;
-
-  if (!bytes) return;
 
   j0 = x->input[0];
   j1 = x->input[1];
