a73979a0938552499c58f56f619a87048fd3bfb2
Sebastian Huber <sebastian.huber@embedded-brains.de>
2026-07-10 06:32:32 +0200

    validation: Test memchr()
    Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

---
+++ testsuites/validation/tc-string-memchr.c
@@ -0,0 +1,260 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+/**
+ * @file
+ *
+ * @ingroup CStringReqMemchr
+ */
+
+/*
+ * Copyright (C) 2026 embedded brains GmbH & Co. KG
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * This file is part of the RTEMS quality process and was automatically
+ * generated.  If you find something that needs to be fixed or
+ * worded better please post a report or patch to an RTEMS mailing list
+ * or raise a bug report:
+ *
+ * https://www.rtems.org/bugs.html
+ *
+ * For information on updating and regenerating please refer to the How-To
+ * section in the Software Requirements Engineering chapter of the
+ * RTEMS Software Engineering manual.  The manual is provided as a part of
+ * a release.  For development sources please refer to the online
+ * documentation at:
+ *
+ * https://docs.rtems.org
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <string.h>
+
+#include <rtems/test.h>
+
+/**
+ * @defgroup CStringReqMemchr spec:/c/string/req/memchr
+ *
+ * @{
+ */
+
+typedef enum {
+  CStringReqMemchr_Pre_Status_Ok,
+  CStringReqMemchr_Pre_Status_NA
+} CStringReqMemchr_Pre_Status;
+
+typedef enum {
+  CStringReqMemchr_Post_Status_Ok,
+  CStringReqMemchr_Post_Status_NA
+} CStringReqMemchr_Post_Status;
+
+typedef struct {
+  uint8_t Skip : 1;
+  uint8_t Pre_Status_NA : 1;
+  uint8_t Post_Status : 1;
+} CStringReqMemchr_Entry;
+
+/**
+ * @brief Test context for spec:/c/string/req/memchr test case.
+ */
+typedef struct {
+  /**
+   * @brief This member specifies the `s` parameter value.
+   */
+  const void *s;
+
+  /**
+   * @brief This member specifies the `c` parameter value.
+   */
+  int c;
+
+  /**
+   * @brief This member specifies the `n` parameter value.
+   */
+  size_t n;
+
+  /**
+   * @brief This member contains the return value.
+   */
+  void *retval;
+
+  struct {
+    /**
+     * @brief This member defines the pre-condition states for the next action.
+     */
+    size_t pcs[ 1 ];
+
+    /**
+     * @brief If this member is true, then the test action loop is executed.
+     */
+    bool in_action_loop;
+
+    /**
+     * @brief This member contains the next transition map index.
+     */
+    size_t index;
+
+    /**
+     * @brief This member contains the current transition map entry.
+     */
+    CStringReqMemchr_Entry entry;
+
+    /**
+     * @brief If this member is true, then the current transition variant
+     *   should be skipped.
+     */
+    bool skip;
+  } Map;
+} CStringReqMemchr_Context;
+
+static CStringReqMemchr_Context
+  CStringReqMemchr_Instance;
+
+static const char * const CStringReqMemchr_PreDesc_Status[] = {
+  "Ok",
+  "NA"
+};
+
+static const char * const * const CStringReqMemchr_PreDesc[] = {
+  CStringReqMemchr_PreDesc_Status,
+  NULL
+};
+
+static void CStringReqMemchr_Pre_Status_Prepare(
+  CStringReqMemchr_Pre_Status state
+)
+{
+  switch ( state ) {
+    case CStringReqMemchr_Pre_Status_Ok: {
+      /*
+       * TODO
+       */
+      /* TODO */
+      break;
+    }
+
+    case CStringReqMemchr_Pre_Status_NA:
+      break;
+  }
+}
+
+static void CStringReqMemchr_Post_Status_Check(
+  CStringReqMemchr_Post_Status state
+)
+{
+  switch ( state ) {
+    case CStringReqMemchr_Post_Status_Ok: {
+      /*
+       * TODO
+       */
+      /* TODO */
+      break;
+    }
+
+    case CStringReqMemchr_Post_Status_NA:
+      break;
+  }
+}
+
+static void CStringReqMemchr_Action( CStringReqMemchr_Context *ctx )
+{
+  ctx->retval = memchr( ctx->s, ctx->c, ctx->n );
+}
+
+static const CStringReqMemchr_Entry
+CStringReqMemchr_Entries[] = {
+  { 0, 0, CStringReqMemchr_Post_Status_Ok }
+};
+
+static const uint8_t
+CStringReqMemchr_Map[] = {
+  0
+};
+
+static size_t CStringReqMemchr_Scope( void *arg, char *buf, size_t n )
+{
+  CStringReqMemchr_Context *ctx;
+
+  ctx = arg;
+
+  if ( ctx->Map.in_action_loop ) {
+    return T_get_scope( CStringReqMemchr_PreDesc, buf, n, ctx->Map.pcs );
+  }
+
+  return 0;
+}
+
+static T_fixture CStringReqMemchr_Fixture = {
+  .setup = NULL,
+  .stop = NULL,
+  .teardown = NULL,
+  .scope = CStringReqMemchr_Scope,
+  .initial_context = &CStringReqMemchr_Instance
+};
+
+static inline CStringReqMemchr_Entry CStringReqMemchr_PopEntry(
+  CStringReqMemchr_Context *ctx
+)
+{
+  size_t index;
+
+  index = ctx->Map.index;
+  ctx->Map.index = index + 1;
+  return CStringReqMemchr_Entries[
+    CStringReqMemchr_Map[ index ]
+  ];
+}
+
+static void CStringReqMemchr_TestVariant( CStringReqMemchr_Context *ctx )
+{
+  CStringReqMemchr_Pre_Status_Prepare( ctx->Map.pcs[ 0 ] );
+  CStringReqMemchr_Action( ctx );
+  CStringReqMemchr_Post_Status_Check( ctx->Map.entry.Post_Status );
+}
+
+/**
+ * @fn void T_case_body_CStringReqMemchr( void )
+ */
+T_TEST_CASE_FIXTURE( CStringReqMemchr, &CStringReqMemchr_Fixture )
+{
+  CStringReqMemchr_Context *ctx;
+
+  ctx = T_fixture_context();
+  ctx->Map.in_action_loop = true;
+  ctx->Map.index = 0;
+
+  for (
+    ctx->Map.pcs[ 0 ] = CStringReqMemchr_Pre_Status_Ok;
+    ctx->Map.pcs[ 0 ] < CStringReqMemchr_Pre_Status_NA;
+    ++ctx->Map.pcs[ 0 ]
+  ) {
+    ctx->Map.entry = CStringReqMemchr_PopEntry( ctx );
+    CStringReqMemchr_TestVariant( ctx );
+  }
+}
+
+/** @} */
