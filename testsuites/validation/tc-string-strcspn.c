3a15ab084290ad4f97656feffc2959f11fe34b18
Sebastian Huber <sebastian.huber@embedded-brains.de>
2026-07-10 06:32:40 +0200

    validation: Add strcspn() test case
    Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

--- testsuites/validation/tc-string-strcspn.c
+++ testsuites/validation/tc-string-strcspn.c
@@ -63,19 +63,44 @@
  */
 
 typedef enum {
-  CStringReqStrcspn_Pre_Status_Ok,
-  CStringReqStrcspn_Pre_Status_NA
-} CStringReqStrcspn_Pre_Status;
+  CStringReqStrcspn_Pre_S1_Empty,
+  CStringReqStrcspn_Pre_S1_NonEmpty,
+  CStringReqStrcspn_Pre_S1_NA
+} CStringReqStrcspn_Pre_S1;
 
 typedef enum {
-  CStringReqStrcspn_Post_Status_Ok,
-  CStringReqStrcspn_Post_Status_NA
-} CStringReqStrcspn_Post_Status;
+  CStringReqStrcspn_Pre_S2_Empty,
+  CStringReqStrcspn_Pre_S2_NonEmpty,
+  CStringReqStrcspn_Pre_S2_NA
+} CStringReqStrcspn_Pre_S2;
+
+typedef enum {
+  CStringReqStrcspn_Pre_Match_Found,
+  CStringReqStrcspn_Pre_Match_NotFound,
+  CStringReqStrcspn_Pre_Match_NA
+} CStringReqStrcspn_Pre_Match;
+
+typedef enum {
+  CStringReqStrcspn_Pre_MatchPosition_First,
+  CStringReqStrcspn_Pre_MatchPosition_S1Later,
+  CStringReqStrcspn_Pre_MatchPosition_S2Later,
+  CStringReqStrcspn_Pre_MatchPosition_NA
+} CStringReqStrcspn_Pre_MatchPosition;
+
+typedef enum {
+  CStringReqStrcspn_Post_Result_Zero,
+  CStringReqStrcspn_Post_Result_Partial,
+  CStringReqStrcspn_Post_Result_Full,
+  CStringReqStrcspn_Post_Result_NA
+} CStringReqStrcspn_Post_Result;
 
 typedef struct {
   uint8_t Skip : 1;
-  uint8_t Pre_Status_NA : 1;
-  uint8_t Post_Status : 1;
+  uint8_t Pre_S1_NA : 1;
+  uint8_t Pre_S2_NA : 1;
+  uint8_t Pre_Match_NA : 1;
+  uint8_t Pre_MatchPosition_NA : 1;
+  uint8_t Post_Result : 2;
 } CStringReqStrcspn_Entry;
 
 /**
@@ -83,25 +108,47 @@ typedef struct {
  */
 typedef struct {
   /**
-   * @brief This member specifies the `s1` parameter value.
+   * @brief This member specifies the ``s1`` parameter value.
    */
-  const char * s1;
+  const char *s1;
 
   /**
-   * @brief This member specifies the `s2` parameter value.
+   * @brief This member specifies the ``s2`` parameter value.
    */
-  const char * s2;
+  const char *s2;
 
   /**
    * @brief This member contains the return value.
    */
   size_t retval;
 
+  /**
+   * @brief This member contains the expected return value for the Partial
+   *   post-condition state.
+   */
+  size_t expected_len;
+
+  /**
+   * @brief This member provides the ``s1`` buffer.
+   */
+  char s1_buf[ 32 ];
+
+  /**
+   * @brief This member provides the ``s2`` buffer.
+   */
+  char s2_buf[ 16 ];
+
   struct {
+    /**
+     * @brief This member defines the pre-condition indices for the next
+     *   action.
+     */
+    size_t pci[ 4 ];
+
     /**
      * @brief This member defines the pre-condition states for the next action.
      */
-    size_t pcs[ 1 ];
+    size_t pcs[ 4 ];
 
     /**
      * @brief If this member is true, then the test action loop is executed.
@@ -129,50 +176,214 @@ typedef struct {
 static CStringReqStrcspn_Context
   CStringReqStrcspn_Instance;
 
-static const char * const CStringReqStrcspn_PreDesc_Status[] = {
-  "Ok",
+static const char * const CStringReqStrcspn_PreDesc_S1[] = {
+  "Empty",
+  "NonEmpty",
+  "NA"
+};
+
+static const char * const CStringReqStrcspn_PreDesc_S2[] = {
+  "Empty",
+  "NonEmpty",
+  "NA"
+};
+
+static const char * const CStringReqStrcspn_PreDesc_Match[] = {
+  "Found",
+  "NotFound",
+  "NA"
+};
+
+static const char * const CStringReqStrcspn_PreDesc_MatchPosition[] = {
+  "First",
+  "S1Later",
+  "S2Later",
   "NA"
 };
 
 static const char * const * const CStringReqStrcspn_PreDesc[] = {
-  CStringReqStrcspn_PreDesc_Status,
+  CStringReqStrcspn_PreDesc_S1,
+  CStringReqStrcspn_PreDesc_S2,
+  CStringReqStrcspn_PreDesc_Match,
+  CStringReqStrcspn_PreDesc_MatchPosition,
   NULL
 };
 
-static void CStringReqStrcspn_Pre_Status_Prepare(
-  CStringReqStrcspn_Pre_Status state
+static void CStringReqStrcspn_Pre_S1_Prepare(
+  CStringReqStrcspn_Context *ctx,
+  CStringReqStrcspn_Pre_S1   state
 )
 {
   switch ( state ) {
-    case CStringReqStrcspn_Pre_Status_Ok: {
+    case CStringReqStrcspn_Pre_S1_Empty: {
       /*
-       * TODO
+       * While the string referenced by ``s1`` is empty.
        */
-      /* TODOD */
+      ctx->s1_buf[ 0 ] = '\0';
       break;
     }
 
-    case CStringReqStrcspn_Pre_Status_NA:
+    case CStringReqStrcspn_Pre_S1_NonEmpty: {
+      /*
+       * While the string referenced by ``s1`` is not empty.
+       */
+      strcpy( ctx->s1_buf, "XXXXX" );
+      break;
+    }
+
+    case CStringReqStrcspn_Pre_S1_NA:
       break;
   }
 }
 
-static void CStringReqStrcspn_Post_Status_Check(
-  CStringReqStrcspn_Post_Status state
+static void CStringReqStrcspn_Pre_S2_Prepare(
+  CStringReqStrcspn_Context *ctx,
+  CStringReqStrcspn_Pre_S2   state
 )
 {
   switch ( state ) {
-    case CStringReqStrcspn_Post_Status_Ok: {
+    case CStringReqStrcspn_Pre_S2_Empty: {
       /*
-       * TODO
+       * While the string referenced by ``s2`` is empty.
        */
-      /* TODOD */
+      ctx->s2_buf[ 0 ] = '\0';
       break;
     }
 
-    case CStringReqStrcspn_Post_Status_NA:
+    case CStringReqStrcspn_Pre_S2_NonEmpty: {
+      /*
+       * While the string referenced by ``s2`` is not empty.
+       */
+      strcpy( ctx->s2_buf, "ZZZ" );
+      break;
+    }
+
+    case CStringReqStrcspn_Pre_S2_NA:
       break;
   }
+}
+
+static void CStringReqStrcspn_Pre_Match_Prepare(
+  CStringReqStrcspn_Context  *ctx,
+  CStringReqStrcspn_Pre_Match state
+)
+{
+  switch ( state ) {
+    case CStringReqStrcspn_Pre_Match_Found: {
+      /*
+       * While a character of the string referenced by ``s1`` occurs in the
+       * string referenced by ``s2``.
+       */
+      ctx->s1_buf[ 0 ] = 'Y';
+      ctx->s2_buf[ 0 ] = 'Y';
+      break;
+    }
+
+    case CStringReqStrcspn_Pre_Match_NotFound: {
+      /*
+       * While no character of the string referenced by ``s1`` occurs in the
+       * string referenced by ``s2``.
+       */
+      /* The default fill values of the buffers do not overlap */
+      break;
+    }
+
+    case CStringReqStrcspn_Pre_Match_NA:
+      break;
+  }
+}
+
+static void CStringReqStrcspn_Pre_MatchPosition_Prepare(
+  CStringReqStrcspn_Context          *ctx,
+  CStringReqStrcspn_Pre_MatchPosition state
+)
+{
+  switch ( state ) {
+    case CStringReqStrcspn_Pre_MatchPosition_First: {
+      /*
+       * While the first character of the string referenced by ``s1`` matches
+       * the first character of the string referenced by ``s2``.
+       */
+      /* Already set up generically by Match: Found */
+      break;
+    }
+
+    case CStringReqStrcspn_Pre_MatchPosition_S1Later: {
+      /*
+       * While a character other than the first character of the string
+       * referenced by ``s1`` is the first character of that string to match
+       * the first character of the string referenced by ``s2``.
+       */
+      ctx->s1_buf[ 0 ] = 'X';
+      ctx->s1_buf[ 2 ] = 'Y';
+      ctx->expected_len = 2;
+      break;
+    }
+
+    case CStringReqStrcspn_Pre_MatchPosition_S2Later: {
+      /*
+       * While the first character of the string referenced by ``s1`` matches a
+       * character other than the first character of the string referenced by
+       * ``s2``.
+       */
+      ctx->s2_buf[ 0 ] = 'Z';
+      ctx->s2_buf[ 2 ] = 'Y';
+      break;
+    }
+
+    case CStringReqStrcspn_Pre_MatchPosition_NA:
+      break;
+  }
+}
+
+static void CStringReqStrcspn_Post_Result_Check(
+  CStringReqStrcspn_Context    *ctx,
+  CStringReqStrcspn_Post_Result state
+)
+{
+  switch ( state ) {
+    case CStringReqStrcspn_Post_Result_Zero: {
+      /*
+       * The return value of strcspn() shall be equal to zero.
+       */
+      T_eq_uint( ctx->retval, 0 );
+      break;
+    }
+
+    case CStringReqStrcspn_Post_Result_Partial: {
+      /*
+       * The return value of strcspn() shall be greater than zero, while the
+       * return value of strcspn() is less than the length of the string
+       * referenced by ``s1``.
+       */
+      T_eq_uint( ctx->retval, ctx->expected_len );
+      T_gt_uint( ctx->retval, 0 );
+      T_lt_uint( ctx->retval, strlen( ctx->s1 ) );
+      break;
+    }
+
+    case CStringReqStrcspn_Post_Result_Full: {
+      /*
+       * The return value of strcspn() shall be equal to the length of the
+       * string referenced by ``s1``.
+       */
+      T_eq_uint( ctx->retval, strlen( ctx->s1 ) );
+      break;
+    }
+
+    case CStringReqStrcspn_Post_Result_NA:
+      break;
+  }
+}
+
+static void CStringReqStrcspn_Prepare( CStringReqStrcspn_Context *ctx )
+{
+  memset( ctx->s1_buf, 0, sizeof( ctx->s1_buf ) );
+  memset( ctx->s2_buf, 0, sizeof( ctx->s2_buf ) );
+  ctx->s1 = ctx->s1_buf;
+  ctx->s2 = ctx->s2_buf;
+  ctx->retval = 0;
+  ctx->expected_len = 0;
 }
 
 static void CStringReqStrcspn_Action( CStringReqStrcspn_Context *ctx )
@@ -182,12 +393,16 @@ static void CStringReqStrcspn_Action( CStringReqStrcspn_Context *ctx )
 
 static const CStringReqStrcspn_Entry
 CStringReqStrcspn_Entries[] = {
-  { 0, 0, CStringReqStrcspn_Post_Status_Ok }
+  { 0, 0, 1, 1, 1, CStringReqStrcspn_Post_Result_Zero },
+  { 0, 0, 0, 1, 1, CStringReqStrcspn_Post_Result_Full },
+  { 0, 0, 0, 0, 1, CStringReqStrcspn_Post_Result_Full },
+  { 0, 0, 0, 0, 0, CStringReqStrcspn_Post_Result_Zero },
+  { 0, 0, 0, 0, 0, CStringReqStrcspn_Post_Result_Partial }
 };
 
 static const uint8_t
 CStringReqStrcspn_Map[] = {
-  0
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 3, 4, 3, 2, 2, 2
 };
 
 static size_t CStringReqStrcspn_Scope( void *arg, char *buf, size_t n )
@@ -224,11 +439,39 @@ static inline CStringReqStrcspn_Entry CStringReqStrcspn_PopEntry(
   ];
 }
 
+static void CStringReqStrcspn_SetPreConditionStates(
+  CStringReqStrcspn_Context *ctx
+)
+{
+  ctx->Map.pcs[ 0 ] = ctx->Map.pci[ 0 ];
+
+  if ( ctx->Map.entry.Pre_S2_NA ) {
+    ctx->Map.pcs[ 1 ] = CStringReqStrcspn_Pre_S2_NA;
+  } else {
+    ctx->Map.pcs[ 1 ] = ctx->Map.pci[ 1 ];
+  }
+
+  if ( ctx->Map.entry.Pre_Match_NA ) {
+    ctx->Map.pcs[ 2 ] = CStringReqStrcspn_Pre_Match_NA;
+  } else {
+    ctx->Map.pcs[ 2 ] = ctx->Map.pci[ 2 ];
+  }
+
+  if ( ctx->Map.entry.Pre_MatchPosition_NA ) {
+    ctx->Map.pcs[ 3 ] = CStringReqStrcspn_Pre_MatchPosition_NA;
+  } else {
+    ctx->Map.pcs[ 3 ] = ctx->Map.pci[ 3 ];
+  }
+}
+
 static void CStringReqStrcspn_TestVariant( CStringReqStrcspn_Context *ctx )
 {
-  CStringReqStrcspn_Pre_Status_Prepare( ctx->Map.pcs[ 0 ] );
+  CStringReqStrcspn_Pre_S1_Prepare( ctx, ctx->Map.pcs[ 0 ] );
+  CStringReqStrcspn_Pre_S2_Prepare( ctx, ctx->Map.pcs[ 1 ] );
+  CStringReqStrcspn_Pre_Match_Prepare( ctx, ctx->Map.pcs[ 2 ] );
+  CStringReqStrcspn_Pre_MatchPosition_Prepare( ctx, ctx->Map.pcs[ 3 ] );
   CStringReqStrcspn_Action( ctx );
-  CStringReqStrcspn_Post_Status_Check( ctx->Map.entry.Post_Status );
+  CStringReqStrcspn_Post_Result_Check( ctx, ctx->Map.entry.Post_Result );
 }
 
 /**
@@ -243,12 +486,32 @@ T_TEST_CASE_FIXTURE( CStringReqStrcspn, &CStringReqStrcspn_Fixture )
   ctx->Map.index = 0;
 
   for (
-    ctx->Map.pcs[ 0 ] = CStringReqStrcspn_Pre_Status_Ok;
-    ctx->Map.pcs[ 0 ] < CStringReqStrcspn_Pre_Status_NA;
-    ++ctx->Map.pcs[ 0 ]
+    ctx->Map.pci[ 0 ] = CStringReqStrcspn_Pre_S1_Empty;
+    ctx->Map.pci[ 0 ] < CStringReqStrcspn_Pre_S1_NA;
+    ++ctx->Map.pci[ 0 ]
   ) {
-    ctx->Map.entry = CStringReqStrcspn_PopEntry( ctx );
-    CStringReqStrcspn_TestVariant( ctx );
+    for (
+      ctx->Map.pci[ 1 ] = CStringReqStrcspn_Pre_S2_Empty;
+      ctx->Map.pci[ 1 ] < CStringReqStrcspn_Pre_S2_NA;
+      ++ctx->Map.pci[ 1 ]
+    ) {
+      for (
+        ctx->Map.pci[ 2 ] = CStringReqStrcspn_Pre_Match_Found;
+        ctx->Map.pci[ 2 ] < CStringReqStrcspn_Pre_Match_NA;
+        ++ctx->Map.pci[ 2 ]
+      ) {
+        for (
+          ctx->Map.pci[ 3 ] = CStringReqStrcspn_Pre_MatchPosition_First;
+          ctx->Map.pci[ 3 ] < CStringReqStrcspn_Pre_MatchPosition_NA;
+          ++ctx->Map.pci[ 3 ]
+        ) {
+          ctx->Map.entry = CStringReqStrcspn_PopEntry( ctx );
+          CStringReqStrcspn_SetPreConditionStates( ctx );
+          CStringReqStrcspn_Prepare( ctx );
+          CStringReqStrcspn_TestVariant( ctx );
+        }
+      }
+    }
   }
 }
 
