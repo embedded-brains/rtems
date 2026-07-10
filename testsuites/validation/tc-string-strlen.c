48f92ff2c8057518b7dd2ac6aedbfdf4f632b20e
Sebastian Huber <sebastian.huber@embedded-brains.de>
2026-07-10 06:32:40 +0200

    validation: Add strlen() test case
    Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

--- testsuites/validation/tc-string-strlen.c
+++ testsuites/validation/tc-string-strlen.c
@@ -52,6 +52,7 @@
 #include "config.h"
 #endif
 
+#include <rtems.h>
 #include <string.h>
 
 #include <rtems/test.h>
@@ -63,19 +64,27 @@
  */
 
 typedef enum {
-  CStringReqStrlen_Pre_Status_Ok,
-  CStringReqStrlen_Pre_Status_NA
-} CStringReqStrlen_Pre_Status;
+  CStringReqStrlen_Pre_Alignment_Aligned,
+  CStringReqStrlen_Pre_Alignment_Unaligned,
+  CStringReqStrlen_Pre_Alignment_NA
+} CStringReqStrlen_Pre_Alignment;
 
 typedef enum {
-  CStringReqStrlen_Post_Status_Ok,
-  CStringReqStrlen_Post_Status_NA
-} CStringReqStrlen_Post_Status;
+  CStringReqStrlen_Pre_MatchPosition_FirstChunk,
+  CStringReqStrlen_Pre_MatchPosition_SecondChunk,
+  CStringReqStrlen_Pre_MatchPosition_NA
+} CStringReqStrlen_Pre_MatchPosition;
+
+typedef enum {
+  CStringReqStrlen_Post_Result_Length,
+  CStringReqStrlen_Post_Result_NA
+} CStringReqStrlen_Post_Result;
 
 typedef struct {
   uint8_t Skip : 1;
-  uint8_t Pre_Status_NA : 1;
-  uint8_t Post_Status : 1;
+  uint8_t Pre_Alignment_NA : 1;
+  uint8_t Pre_MatchPosition_NA : 1;
+  uint8_t Post_Result : 1;
 } CStringReqStrlen_Entry;
 
 /**
@@ -83,7 +92,7 @@ typedef struct {
  */
 typedef struct {
   /**
-   * @brief This member specifies the `s` parameter value.
+   * @brief This member specifies the ``s`` parameter value.
    */
   const char *s;
 
@@ -92,11 +101,27 @@ typedef struct {
    */
   size_t retval;
 
+  /**
+   * @brief This member contains the expected return value.
+   */
+  size_t expected_len;
+
+  /**
+   * @brief This member contains the offset of the guarded ``s`` region from
+   *   the start of the source buffer.
+   */
+  size_t offset;
+
+  /**
+   * @brief This member provides the source buffer.
+   */
+  unsigned char s_buf[ 8 * sizeof( long ) ] RTEMS_ALIGNED( sizeof( long ) );
+
   struct {
     /**
      * @brief This member defines the pre-condition states for the next action.
      */
-    size_t pcs[ 1 ];
+    size_t pcs[ 2 ];
 
     /**
      * @brief If this member is true, then the test action loop is executed.
@@ -124,50 +149,113 @@ typedef struct {
 static CStringReqStrlen_Context
   CStringReqStrlen_Instance;
 
-static const char * const CStringReqStrlen_PreDesc_Status[] = {
-  "Ok",
+static const char * const CStringReqStrlen_PreDesc_Alignment[] = {
+  "Aligned",
+  "Unaligned",
+  "NA"
+};
+
+static const char * const CStringReqStrlen_PreDesc_MatchPosition[] = {
+  "FirstChunk",
+  "SecondChunk",
   "NA"
 };
 
 static const char * const * const CStringReqStrlen_PreDesc[] = {
-  CStringReqStrlen_PreDesc_Status,
+  CStringReqStrlen_PreDesc_Alignment,
+  CStringReqStrlen_PreDesc_MatchPosition,
   NULL
 };
 
-static void CStringReqStrlen_Pre_Status_Prepare(
-  CStringReqStrlen_Pre_Status state
+static void CStringReqStrlen_Pre_Alignment_Prepare(
+  CStringReqStrlen_Context      *ctx,
+  CStringReqStrlen_Pre_Alignment state
 )
 {
   switch ( state ) {
-    case CStringReqStrlen_Pre_Status_Ok: {
+    case CStringReqStrlen_Pre_Alignment_Aligned: {
       /*
-       * TODO
+       * While the ``s`` parameter is aligned on a `long` integer boundary of
+       * the target architecture.
        */
-      /* TODO */
+      ctx->offset = sizeof( long );
+      ctx->s = (const char *) ( ctx->s_buf + ctx->offset );
       break;
     }
 
-    case CStringReqStrlen_Pre_Status_NA:
+    case CStringReqStrlen_Pre_Alignment_Unaligned: {
+      /*
+       * While the ``s`` parameter is not aligned on a `long` integer boundary
+       * of the target architecture.
+       */
+      ctx->offset = sizeof( long ) + 1;
+      ctx->s = (const char *) ( ctx->s_buf + ctx->offset );
+      break;
+    }
+
+    case CStringReqStrlen_Pre_Alignment_NA:
       break;
   }
 }
 
-static void CStringReqStrlen_Post_Status_Check(
-  CStringReqStrlen_Post_Status state
+static void CStringReqStrlen_Pre_MatchPosition_Prepare(
+  CStringReqStrlen_Context          *ctx,
+  CStringReqStrlen_Pre_MatchPosition state
 )
 {
   switch ( state ) {
-    case CStringReqStrlen_Post_Status_Ok: {
+    case CStringReqStrlen_Pre_MatchPosition_FirstChunk: {
       /*
-       * TODO
+       * While the terminating null character occurs within the first aligned
+       * `long` integer sized chunk of the string referenced by ``s``.
        */
-      /* TODO */
+      ctx->s_buf[ ctx->offset ] = 0;
+      ctx->expected_len = 0;
       break;
     }
 
-    case CStringReqStrlen_Post_Status_NA:
+    case CStringReqStrlen_Pre_MatchPosition_SecondChunk: {
+      /*
+       * While the terminating null character occurs within the second aligned
+       * `long` integer sized chunk of the string referenced by ``s``.
+       */
+      ctx->s_buf[ ctx->offset + sizeof( long ) ] = 0;
+      ctx->expected_len = sizeof( long );
+      break;
+    }
+
+    case CStringReqStrlen_Pre_MatchPosition_NA:
       break;
   }
+}
+
+static void CStringReqStrlen_Post_Result_Check(
+  CStringReqStrlen_Context    *ctx,
+  CStringReqStrlen_Post_Result state
+)
+{
+  switch ( state ) {
+    case CStringReqStrlen_Post_Result_Length: {
+      /*
+       * The return value of strlen() shall be equal to the length of the
+       * string referenced by ``s``.
+       */
+      T_eq_uint( ctx->retval, ctx->expected_len );
+      break;
+    }
+
+    case CStringReqStrlen_Post_Result_NA:
+      break;
+  }
+}
+
+static void CStringReqStrlen_Prepare( CStringReqStrlen_Context *ctx )
+{
+  memset( ctx->s_buf, 0x41, sizeof( ctx->s_buf ) );
+  ctx->offset = sizeof( long );
+  ctx->s = (const char *) ( ctx->s_buf + ctx->offset );
+  ctx->retval = 0;
+  ctx->expected_len = 0;
 }
 
 static void CStringReqStrlen_Action( CStringReqStrlen_Context *ctx )
@@ -177,12 +265,12 @@ static void CStringReqStrlen_Action( CStringReqStrlen_Context *ctx )
 
 static const CStringReqStrlen_Entry
 CStringReqStrlen_Entries[] = {
-  { 0, 0, CStringReqStrlen_Post_Status_Ok }
+  { 0, 0, 0, CStringReqStrlen_Post_Result_Length }
 };
 
 static const uint8_t
 CStringReqStrlen_Map[] = {
-  0
+  0, 0, 0, 0
 };
 
 static size_t CStringReqStrlen_Scope( void *arg, char *buf, size_t n )
@@ -221,9 +309,10 @@ static inline CStringReqStrlen_Entry CStringReqStrlen_PopEntry(
 
 static void CStringReqStrlen_TestVariant( CStringReqStrlen_Context *ctx )
 {
-  CStringReqStrlen_Pre_Status_Prepare( ctx->Map.pcs[ 0 ] );
+  CStringReqStrlen_Pre_Alignment_Prepare( ctx, ctx->Map.pcs[ 0 ] );
+  CStringReqStrlen_Pre_MatchPosition_Prepare( ctx, ctx->Map.pcs[ 1 ] );
   CStringReqStrlen_Action( ctx );
-  CStringReqStrlen_Post_Status_Check( ctx->Map.entry.Post_Status );
+  CStringReqStrlen_Post_Result_Check( ctx, ctx->Map.entry.Post_Result );
 }
 
 /**
@@ -238,12 +327,19 @@ T_TEST_CASE_FIXTURE( CStringReqStrlen, &CStringReqStrlen_Fixture )
   ctx->Map.index = 0;
 
   for (
-    ctx->Map.pcs[ 0 ] = CStringReqStrlen_Pre_Status_Ok;
-    ctx->Map.pcs[ 0 ] < CStringReqStrlen_Pre_Status_NA;
+    ctx->Map.pcs[ 0 ] = CStringReqStrlen_Pre_Alignment_Aligned;
+    ctx->Map.pcs[ 0 ] < CStringReqStrlen_Pre_Alignment_NA;
     ++ctx->Map.pcs[ 0 ]
   ) {
-    ctx->Map.entry = CStringReqStrlen_PopEntry( ctx );
-    CStringReqStrlen_TestVariant( ctx );
+    for (
+      ctx->Map.pcs[ 1 ] = CStringReqStrlen_Pre_MatchPosition_FirstChunk;
+      ctx->Map.pcs[ 1 ] < CStringReqStrlen_Pre_MatchPosition_NA;
+      ++ctx->Map.pcs[ 1 ]
+    ) {
+      ctx->Map.entry = CStringReqStrlen_PopEntry( ctx );
+      CStringReqStrlen_Prepare( ctx );
+      CStringReqStrlen_TestVariant( ctx );
+    }
   }
 }
 
