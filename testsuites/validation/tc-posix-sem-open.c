a1ac3c6abd316f89a0d5b67e461ead6c39905ee8
Sebastian Huber <sebastian.huber@embedded-brains.de>
2026-07-10 06:32:46 +0200

    validation: Test sem_open()
    Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

---
+++ testsuites/validation/tc-posix-sem-open.c
@@ -0,0 +1,255 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+/**
+ * @file
+ *
+ * @ingroup CSemaphoreReqOpen
+ */
+
+/*
+ * Copyright (C) 2026 embedded brains GmbH & Co. KG
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * This file is part of the RTEMS quality process and was automatically
+ * generated.  If you find something that needs to be fixed or
+ * worded better please post a report or patch to an RTEMS mailing list
+ * or raise a bug report:
+ *
+ * https://www.rtems.org/bugs.html
+ *
+ * For information on updating and regenerating please refer to the How-To
+ * section in the Software Requirements Engineering chapter of the
+ * RTEMS Software Engineering manual.  The manual is provided as a part of
+ * a release.  For development sources please refer to the online
+ * documentation at:
+ *
+ * https://docs.rtems.org
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <semaphore.h>
+
+#include <rtems/test.h>
+
+/**
+ * @defgroup CSemaphoreReqOpen spec:/c/semaphore/req/open
+ *
+ * @{
+ */
+
+typedef enum {
+  CSemaphoreReqOpen_Pre_Status_Ok,
+  CSemaphoreReqOpen_Pre_Status_NA
+} CSemaphoreReqOpen_Pre_Status;
+
+typedef enum {
+  CSemaphoreReqOpen_Post_Status_Ok,
+  CSemaphoreReqOpen_Post_Status_NA
+} CSemaphoreReqOpen_Post_Status;
+
+typedef struct {
+  uint8_t Skip : 1;
+  uint8_t Pre_Status_NA : 1;
+  uint8_t Post_Status : 1;
+} CSemaphoreReqOpen_Entry;
+
+/**
+ * @brief Test context for spec:/c/semaphore/req/open test case.
+ */
+typedef struct {
+  /**
+   * @brief This member specifies the `name` parameter value.
+   */
+  const char *name;
+
+  /**
+   * @brief This member specifies the `oflag` parameter value.
+   */
+  int oflag;
+
+  /**
+   * @brief This member contains the return value.
+   */
+  sem_t *retval;
+
+  struct {
+    /**
+     * @brief This member defines the pre-condition states for the next action.
+     */
+    size_t pcs[ 1 ];
+
+    /**
+     * @brief If this member is true, then the test action loop is executed.
+     */
+    bool in_action_loop;
+
+    /**
+     * @brief This member contains the next transition map index.
+     */
+    size_t index;
+
+    /**
+     * @brief This member contains the current transition map entry.
+     */
+    CSemaphoreReqOpen_Entry entry;
+
+    /**
+     * @brief If this member is true, then the current transition variant
+     *   should be skipped.
+     */
+    bool skip;
+  } Map;
+} CSemaphoreReqOpen_Context;
+
+static CSemaphoreReqOpen_Context
+  CSemaphoreReqOpen_Instance;
+
+static const char * const CSemaphoreReqOpen_PreDesc_Status[] = {
+  "Ok",
+  "NA"
+};
+
+static const char * const * const CSemaphoreReqOpen_PreDesc[] = {
+  CSemaphoreReqOpen_PreDesc_Status,
+  NULL
+};
+
+static void CSemaphoreReqOpen_Pre_Status_Prepare(
+  CSemaphoreReqOpen_Pre_Status state
+)
+{
+  switch ( state ) {
+    case CSemaphoreReqOpen_Pre_Status_Ok: {
+      /*
+       * TODO
+       */
+      /* TODO */
+      break;
+    }
+
+    case CSemaphoreReqOpen_Pre_Status_NA:
+      break;
+  }
+}
+
+static void CSemaphoreReqOpen_Post_Status_Check(
+  CSemaphoreReqOpen_Post_Status state
+)
+{
+  switch ( state ) {
+    case CSemaphoreReqOpen_Post_Status_Ok: {
+      /*
+       * TODO
+       */
+      /* TODO */
+      break;
+    }
+
+    case CSemaphoreReqOpen_Post_Status_NA:
+      break;
+  }
+}
+
+static void CSemaphoreReqOpen_Action( CSemaphoreReqOpen_Context *ctx )
+{
+  ctx->retval = sem_open( ctx->name, ctx->oflag );
+}
+
+static const CSemaphoreReqOpen_Entry
+CSemaphoreReqOpen_Entries[] = {
+  { 0, 0, CSemaphoreReqOpen_Post_Status_Ok }
+};
+
+static const uint8_t
+CSemaphoreReqOpen_Map[] = {
+  0
+};
+
+static size_t CSemaphoreReqOpen_Scope( void *arg, char *buf, size_t n )
+{
+  CSemaphoreReqOpen_Context *ctx;
+
+  ctx = arg;
+
+  if ( ctx->Map.in_action_loop ) {
+    return T_get_scope( CSemaphoreReqOpen_PreDesc, buf, n, ctx->Map.pcs );
+  }
+
+  return 0;
+}
+
+static T_fixture CSemaphoreReqOpen_Fixture = {
+  .setup = NULL,
+  .stop = NULL,
+  .teardown = NULL,
+  .scope = CSemaphoreReqOpen_Scope,
+  .initial_context = &CSemaphoreReqOpen_Instance
+};
+
+static inline CSemaphoreReqOpen_Entry CSemaphoreReqOpen_PopEntry(
+  CSemaphoreReqOpen_Context *ctx
+)
+{
+  size_t index;
+
+  index = ctx->Map.index;
+  ctx->Map.index = index + 1;
+  return CSemaphoreReqOpen_Entries[
+    CSemaphoreReqOpen_Map[ index ]
+  ];
+}
+
+static void CSemaphoreReqOpen_TestVariant( CSemaphoreReqOpen_Context *ctx )
+{
+  CSemaphoreReqOpen_Pre_Status_Prepare( ctx->Map.pcs[ 0 ] );
+  CSemaphoreReqOpen_Action( ctx );
+  CSemaphoreReqOpen_Post_Status_Check( ctx->Map.entry.Post_Status );
+}
+
+/**
+ * @fn void T_case_body_CSemaphoreReqOpen( void )
+ */
+T_TEST_CASE_FIXTURE( CSemaphoreReqOpen, &CSemaphoreReqOpen_Fixture )
+{
+  CSemaphoreReqOpen_Context *ctx;
+
+  ctx = T_fixture_context();
+  ctx->Map.in_action_loop = true;
+  ctx->Map.index = 0;
+
+  for (
+    ctx->Map.pcs[ 0 ] = CSemaphoreReqOpen_Pre_Status_Ok;
+    ctx->Map.pcs[ 0 ] < CSemaphoreReqOpen_Pre_Status_NA;
+    ++ctx->Map.pcs[ 0 ]
+  ) {
+    ctx->Map.entry = CSemaphoreReqOpen_PopEntry( ctx );
+    CSemaphoreReqOpen_TestVariant( ctx );
+  }
+}
+
+/** @} */
